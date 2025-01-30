88909b5cd444bf5b47669ce1a0022ff643c0864e
KachhadiyaGautam027 <kachhadiyagautam54@gmail.com>
2025-01-30 21:00:03 +0530

    mongodb connection and signup function commit

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,27 @@
 const express = require('express');
-const app = express()
+const mongoose = require('mongoose');
+const app = express();
 const port = 5000;
-const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://gautamkachhadiya:wqaNdRKUaVklJ74Q@cluster0.mjras.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+require('./models/model')
+app.use(express.json())
+app.use(require("./routes/auth"))
+// Import MongoDB URL
+const mongoUrl = require('./keys');
 
+// Connect to MongoDB
+mongoose.connect(mongoUrl);
+
+// MongoDB connection events
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose successfully connected to MongoDB Atlas');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+// Start Express server
 app.listen(port, () => {
-    console.log("server is running on port" + " " + port)
-})
+  console.log(`Server is running on port ${port}`);
+});
--- backend/keys.js
+++ backend/keys.js
@@ -1,1 +1,3 @@
-const mongoUrl = "mongodb+srv://gautamkachhadiya:wqaNdRKUaVklJ74Q@cluster0.mjras.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoUrl = "mongodb+srv://gautamkachhadiya:lbkMREDYdzp8ol5z@cluster0.8vd7w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+module.exports = mongoUrl;
